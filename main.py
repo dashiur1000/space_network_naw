41ccb60ac8edcfecf733ad99ebea247c2d587844
dudi <dzs101010@gmail.com>
2026-01-28 12:27:17 +0200

    שגיאות קבועות

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from GitHub.space_network_lib import SpaceNetwork
 from space_network_lib import SpaceEntity, Packet
 import time
-from space_network_lib import TemporalInterferenceError, DataCorruptedError
+from space_network_lib import CommsError, TemporalInterferenceError, DataCorruptedError, LinkTerminatedError, OutOfRangeError
 class Satellite(SpaceEntity):
    def __init__(self, name, distance_from_earth):
        super().__init__(name, distance_from_earth)
        self.distance_from_earth = distance_from_earth
    def receive_signal(self, packet: Packet):
        print(f"[{self.name}] received: {packet}")
+
+
+class BrokenConnectionError(CommsError):
+   pass
 
 
 def attempt_transmission(network, packet):
@@ -20,6 +24,12 @@ def attempt_transmission(network, packet):
            time.sleep(2)
        except DataCorruptedError:
            print("Data corrupted, retrying...")
+       except LinkTerminatedError:
+           print("Link lost")
+           raise BrokenConnectionError
+       except OutOfRangeError:
+           print("Target out of range")
+           raise BrokenConnectionError
 
 
 
@@ -32,4 +42,9 @@ Sat1 = Satellite("Sat1", 100)
 Sat2 = Satellite("Sat2", 200)
 my_net = SpaceNetwork(level=3)
 Packet1 = Packet("Hello", Sat1, Sat2)
-attempt_transmission(my_net, Packet1)
+
+
+try:
+   attempt_transmission(my_net, Packet1)
+except BrokenConnectionError:
+   print("Transmission failed")
